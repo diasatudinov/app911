1e8a7da47347ee7dcf9203d64db19bb3c3b99a60
Dias <dias.atudinov@gmail.com>
2024-10-09 16:46:26 +0500

    Hemo models

--- app911/app911/First/HomeView/Model/Category.swift
+++ app911/app911/First/HomeView/Model/Category.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct homeModel: Codable {
+    var activeEvents: Int
+    var totalEvents: Int
+    var pastEvents: Int
+    var moneySpent: Double
+}
+struct Category: Hashable, Codable {
+    var id = UUID()
+    var name: String
+    var events: [Event]
+}
+
+struct Event: Hashable, Codable {
+    var id = UUID()
+    var name: String
+    var date: Date
+    var location: String
+    var status: String
+    //var ticket: Ticket
+}
