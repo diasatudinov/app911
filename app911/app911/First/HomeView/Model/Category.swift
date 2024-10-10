4add561a011e6d72013c3663b38414635a967276
Dias <dias.atudinov@gmail.com>
2024-10-10 22:13:03 +0500

    category model

--- app911/app911/First/HomeView/Model/Category.swift
+++ app911/app911/First/HomeView/Model/Category.swift
@@ -5,7 +5,7 @@
 //  Created by Dias Atudinov on 08.10.2024.
 //
 
-import Foundation
+import SwiftUI
 
 struct homeModel: Codable {
     var activeEvents: Int
@@ -25,5 +25,35 @@ struct Event: Hashable, Codable {
     var date: Date
     var location: String
     var status: String
-    //var ticket: Ticket
+    var ticket: Ticket?
+    var isArchive: Bool {
+        get {
+            let currentDate = Date()
+            let calendar = Calendar.current
+            let orderDateTime = calendar.date(bySettingHour: calendar.component(.hour, from: date),
+                                              minute: calendar.component(.minute, from: date),
+                                              second: 0, of: date) ?? date
+            return orderDateTime < currentDate
+        }
+    }
+}
+
+struct Ticket: Hashable, Codable {
+    var id = UUID()
+    var imageData: Data?
+    var name: String
+    var date: String
+    var location: String
+    var rowPosition: String
+    var price: String
+    
+    var image: UIImage? {
+        get {
+            guard let data = imageData else { return nil }
+            return UIImage(data: data)
+        }
+        set {
+            imageData = newValue?.jpegData(compressionQuality: 1.0)
+        }
+    }
 }
