e9dc97232989db5b533a9e4887c2a2232c23bc57
Dias <dias.atudinov@gmail.com>
2024-10-10 22:12:19 +0500

    new files

---
+++ app911/app911/First/EventsView/View/EventsUIView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsUIView.swift
+//  app911
+//
+//  Created by Dias Atudinov on 10.10.2024.
+//
+
+import SwiftUI
+
+struct EventsUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsUIView()
+}
